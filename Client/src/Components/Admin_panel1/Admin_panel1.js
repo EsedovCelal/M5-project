38042bf8d4718fa551252f6b06e371db43ec59ed
EsedovCelal <esedov.celal@gmail.com>
2023-02-06 17:10:43 +0400

    added server get response

--- Client/src/Components/Admin_panel1/Admin_panel1.js
+++ Client/src/Components/Admin_panel1/Admin_panel1.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Admin_panel1/Admin_panel1.css";
 import circle from "../../icons/circle.svg";
 import { Link } from "react-router-dom";
 function Admin_panel1() {
+  const [coin, setCoin] = useState([]);
+  useEffect(() => {
+    if (coin.length === 0) {
+      //burda pathname budur /description/:id
+      fetch(`http://localhost:3000/getlasttwocoins`)
+        .then((response) => response.json())
+        .then((data) => setCoin(data))
+        .catch((error) => console.error(error));
+    }
+  }, [coin]);
   return (
     <main className="admin_panel1_main">
       <div className="content">
@@ -18,42 +28,23 @@ function Admin_panel1() {
         </div>
       </div>
       <div className="commun_coin_info">
-        <div className="coin_info">
-          <div>
-            <img src="./images/coin.svg" alt="" />
+        {coin.map((coin) => (
+          <div className="coin_info">
+            <div>
+              <img src={coin.linkObserve} alt="" />
+            </div>
+            <div className="text">
+              <h1>{coin.coinname}</h1>
+              <p>{coin.shortDesc}</p>
+            </div>
+            <div>
+              <button>Edit</button>
+            </div>
+            <div>
+              <button>Delete</button>
+            </div>
           </div>
-          <div className="text">
-            <h1>Canadian Beaver</h1>
-            <p>
-              "Canadian beaver". Unique coin with the image of a beaver. Face
-              value - 5 cents. Created under Elizabeth II.
-            </p>
-          </div>
-          <div>
-            <button>Edit</button>
-          </div>
-          <div>
-            <button>Delete</button>
-          </div>
-        </div>
-        <div className="coin_info">
-          <div>
-            <img src="./images/coin.svg" alt="" />
-          </div>
-          <div className="text">
-            <h1>Canadian Beaver</h1>
-            <p>
-              "Canadian beaver". Unique coin with the image of a beaver. Face
-              value - 5 cents. Created under Elizabeth II.
-            </p>
-          </div>
-          <div>
-            <button>Edit</button>
-          </div>
-          <div>
-            <button>Delete</button>
-          </div>
-        </div>
+        ))}
         <div className="add_new_coin">
           <Link to="/adminpanel2">
             <img src={circle} alt="circle" />
--- Server/server.js
+++ Server/server.js
@@ -22,11 +22,23 @@ connection.connect((err) => {
     });
   }
 });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.get("/getlasttwocoins", function (req, res) {
+  // adminpanel1 də son əlavə edilən 2 coin göstərmək üçün fetch
+  connection.query(
+    "SELECT * FROM coin_informations ORDER BY id DESC LIMIT 2;",
+    (err, data) => {
+      if (err) {
+        return res.status(500).send("There is hava a proble");
+      }
+      res.json(data);
+    }
+  );
+});
 app.get("/:keyword", function (req, res) {
+  //bu hissədə categorylər alınır
   const { keyword } = req.params;
   connection.query(
     `SELECT * FROM coin_informations WHERE category = "${keyword} coins";`,
@@ -38,7 +50,9 @@ app.get("/:keyword", function (req, res) {
     }
   );
 });
+
 app.get("/description/:id", function (req, res) {
+  // id yə görə hər bir coin məlumatı alınması üçün atılan fetch
   const { id } = req.params;
   connection.query(
     `SELECT * FROM coin_informations WHERE id = "${id}";`,
@@ -85,6 +99,7 @@ app.get("/description/:id", function (req, res) {
 //   );
 // });
 app.get("/", function (req, res) {
+  //Home page üçün atılan fetch
   connection.query(
     "SELECT * FROM coin_informations_category_name;",
     (err, data) => {
