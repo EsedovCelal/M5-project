ce63ab02fb3196fcec76d927bda14eabe68ac52b
EsedovCelal <esedov.celal@gmail.com>
2023-02-20 14:33:31 +0400

    added pagination in adminpanel1

--- Client/src/Components/Admin_panel1/Admin_panel1.js
+++ Client/src/Components/Admin_panel1/Admin_panel1.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import "../Admin_panel1/Admin_panel1.css";
 import circle from "../../icons/circle.svg";
 import { Link, useNavigate } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 // import jwt from "jsonwebtoken";
 function AdminPanel1() {
   const [coin, setCoin] = useState([]);
   const [inputvalue, setInputvalue] = useState(""); // q = ? seach üçün inputun valuesi bura atılır
   const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(0); // pagination üçün
   useEffect(() => {
     fetch(`http://localhost:3000/adminpanel1/allcoins`)
       .then((response) => response.json())
@@ -45,6 +47,37 @@ function AdminPanel1() {
       })
       .catch((error) => console.error(error));
   };
+  //#region //pagination üçün istifadə edilən kod
+  const PER_PAGE = 3;
+  const offset = currentPage * PER_PAGE;
+  const currentPageData = coin.slice(offset, offset + PER_PAGE).map((coin) => (
+    <div key={coin.id} className="coin_info">
+      <div>
+        <img src={coin.linkObserve} alt="" />
+      </div>
+      <div className="text">
+        <h1>{coin.coinname}</h1>
+        <p>{coin.shortDesc}</p>
+      </div>
+      <div>
+        <Link to={`/adminpanel2/editcoin/${coin.id}`}>
+          <button>Edit</button>
+        </Link>
+        {/* editə basib coini edit etmək üçün adminpanel2 bölməsinə yəni add coin componentinə keçid edir*/}
+      </div>
+      <div>
+        <button id={coin.id} onClick={deleteitem}>
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
+  const pageCount = Math.ceil(coin.length / PER_PAGE);
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+
+  //#endregion
   return (
     <main className="admin_panel1_main">
       <div className="content">
@@ -65,37 +98,30 @@ function AdminPanel1() {
           <br />
         </div>
       </div>
-
       <div className="commun_coin_info">
         {coin.length === 0 ? (
           <div id="admin_panel1_empty_coin">
             <h1>There is no coin</h1>
           </div>
         ) : (
-          <>
-            {coin.map((coin) => (
-              <div key={coin.id} className="coin_info">
-                <div>
-                  <img src={coin.linkObserve} alt="" />
-                </div>
-                <div className="text">
-                  <h1>{coin.coinname}</h1>
-                  <p>{coin.shortDesc}</p>
-                </div>
-                <div>
-                  <Link to={`/adminpanel2/editcoin/${coin.id}`}>
-                    <button>Edit</button>
-                  </Link>
-                  {/* editə basib coini edit etmək üçün adminpanel2 bölməsinə yəni add coin componentinə keçid edir*/}
-                </div>
-                <div>
-                  <button id={coin.id} onClick={deleteitem}>
-                    Delete
-                  </button>
-                </div>
-              </div>
-            ))}
-          </>
+          <div className="admin_panel1_pagination">
+            <div className="admin_panel1_pagination_coins">
+              {currentPageData}
+            </div>
+            <div className="admin_panel1_pagination_reactpaginate">
+              <ReactPaginate
+                previousLabel={"← Previous"}
+                nextLabel={"Next →"}
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+              />
+            </div>
+          </div>
         )}
         <div className="add_new_coin">
           <Link to="/adminpanel2">
